8256aa426dfd1d0471eb520c7bf52388d65d5b2f
sreeraj11 <51787957+sreeraj11@users.noreply.github.com>
2020-11-06 11:16:23 +0530

    Create git-promt.sh

---
+++ git-promt.sh
@@ -0,0 +1,78 @@
+if test -f /etc/profile.d/git-sdk.sh
+then
+	TITLEPREFIX=SDK-${MSYSTEM#MINGW}
+else
+	TITLEPREFIX=$MSYSTEM
+fi
+
+if test -f ~/.config/git/git-prompt.sh
+then
+	. ~/.config/git/git-prompt.sh
+else
+	Color_Off="\[\033[0m\]" 
+	PS1='\[\033]0;${PWD//[^[:ascii:]]/?}\007\]' # set window title             # new line
+	PS1="$PS1"'\[\033[1;32m\]'       # change to green
+	PS1="$PS1"'➜ '
+	PS1="$PS1"' '  
+	PS1="$PS1"'\[\033[1;36m\]'
+	PS1="$PS1"'\W'
+	PS1="$PS1"' '                 # space
+	PS1="$PS1"'\[\033[1;94m\]' 
+	PS1="$PS1"'git:'         # current working folder
+	if test -z "$WINELOADERNOEXEC"
+	then
+		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+		COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+		COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+		COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+		if test -f "$COMPLETION_PATH/git-prompt.sh"
+		then
+			"$COMPLETION_PATH/git-completion.bash"
+			. "$COMPLETION_PATH/git-prompt.sh"
+			PS1="$PS1"'$(__git_ps1 "\[\033[1;94m\](\[\033[1;31m\]%s\[\033[1;94m\])")'
+			PS1="$PS1"' '       # bash function
+		fi
+	fi
+	PS1="$PS1"'\[\033[1;33m\]'
+	PS1="$PS1"'✗ '    # change color  
+	PS1="$PS1"'\[\033[0;37m\]'   
+	       # space
+fi
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+    . /etc/bash_completion
+fi
+
+
+MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
